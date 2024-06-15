3d8069717eb6dfacc3ce03c2a49ef828f8a8873c
ManishPrajapati <ManishPrajapati0511@gmail.com>
2024-06-15 15:28:46 +0530

    remove wrong project

--- src/Components/CalButton.jsx
+++
@@ -1,8 +0,0 @@
-function CalButton({buttonLable, onClickButton}){
-    return (
-        <div className="col-4">
-            <button onClick={onClickButton} data-lable={buttonLable}>{buttonLable}</button>
-        </div>
-    )
-}
-export default CalButton
--- src/Components/Inputbox.jsx
+++
@@ -1,9 +0,0 @@
-function Inputbox ({calVal})
-{
-    return (
-        <div className='col-12'>
-            <input type="text" value={calVal} readOnly/>
-        </div>
-    )
-}
-export default Inputbox
